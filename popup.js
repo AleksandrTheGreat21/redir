b158e657c21223045f9f4ac106cbb88b43385524
AleksandrTheGreat21 <100033042@mvla.net>
2024-06-15 17:54:43 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 document.getElementById('transferUrl').addEventListener('click', () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const currentUrl = tabs[0].url;
-    const chatGPTUrl = `https://chatgpt.com?url=${encodeURIComponent(currentUrl)}`;
+    const chatGPTUrl = `https://youtube.com?url=${encodeURIComponent(currentUrl)}`;
     chrome.tabs.create({ url: chatGPTUrl });
   });
 });
