9e297e350282cd88d90ec58ceff5b1228b786067
AleksandrTheGreat21 <100033042@mvla.net>
2024-06-20 17:14:58 +0000

    fire one!

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  const currentUrl = tab.url;
-  const chatGPTUrl = `https://chatgpt.com?url=${encodeURIComponent(currentUrl)}`;
-  chrome.tabs.create({ url: chatGPTUrl });
-});
--- popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('transferUrl').addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const currentUrl = tabs[0].url;
-    const chatGPTUrl = `https://youtube.com?url=${encodeURIComponent(currentUrl)}`;
-    chrome.tabs.create({ url: chatGPTUrl });
-  });
-});
